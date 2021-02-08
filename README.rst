d039ce793c23d130dd598c49d8a41afe6ddcf811
EDallas <inmsanz89@gmail.com>
2021-02-08 11:53:57 +0000

    First funtional commit

---
+++ README.rst
@@ -0,0 +1,11 @@
+Helpdesk Ticket Timesheet Displacement Check
+=============================================
+
+    This moduele add a check on helpdesk ticket timesheet for displacement control.
+
+Credits
+-------
+* Authors
+    * Aresoltec Canarias S.L.
+* Contributors
+    * Inma Sánchez
---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Helpdesk Ticket Timesheet Displacement Check",
+    'summary': """
+        Add a check on helpdesk ticket timesheet for displacement control""",
+    'author': "Aresoltec Canarias S.L.",
+    'website': "http://www.aresoltec.com",
+    'category': 'Helpdesk',
+    'version': '1.0',
+    'depends': ['helpdesk_mgmt_timesheet'],
+    'data': [
+        'views/views.xml',
+    ],
+}
